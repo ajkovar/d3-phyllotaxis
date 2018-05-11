e3ec97e98740fc9e4e8f3f37d34d437081c087ac
Alex Kovar <ajkovar@gmail.com>
2018-05-11 09:26:39 -0500

    multiple with data

--- client/index.js
+++ client/index.js
@@ -8,17 +8,15 @@ const svg = d3
   .attr('width', width)
   .attr('height', height);
 
-const data = d3
-  .range(10)
-  .map(() => ({x: Math.random() * width, y: Math.random() * height}));
+const data = () =>
+  d3
+    .range(10)
+    .map(() => ({x: Math.random() * width, y: Math.random() * height}));
 
-data.forEach(d => {
-  svg
-    .append('circle')
-    .attr('r', 5)
-    .attr('cx', d.x)
-    .attr('cy', d.y);
-});
+const circles = svg.selectAll('circle').data(data(), (d, i) => i);
 
-const circles = svg.selectAll('circle');
-console.log(circles.size());
+circles
+  .append('circle')
+  .attr('r', 5)
+  .attr('cx', d => d.x)
+  .attr('cy', d => d.y);
